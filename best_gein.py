563f6582429acfa0f797de37872894dd38560dd8
ch <ay-ya@ya.ru>
2022-01-13 07:40:05 +0300

    add vol change

--- best_gein.py
+++ best_gein.py
@@ -34,19 +34,30 @@ while True:
             close_cost_curr = float(response_json[0][4])
             close_cost_past = float(response_json[1][4])
             quote_asset_volume = float(response_json[0][7])
+            quote_asset_volume_past = float(response_json[1][7])
+            number_of_trades = float(response_json[0][8])
+            number_of_trades_past = float(response_json[1][8])
+
             gain = round(100.*((close_cost_curr / close_cost_past) - 1), 2)
+            usd_vol_increase = round(100.*((quote_asset_volume / quote_asset_volume_past) - 1), 2) if quote_asset_volume_past > 0 else 0
+            number_of_trades_increase = round(100.*((number_of_trades / number_of_trades_past) - 1), 2) if number_of_trades_past > 0 else 0
+
 
 
             ts = datetime.utcfromtimestamp(int(ts/1000)).strftime('%Y-%m-%d %H:%M:%S')
 
             ts_end = datetime.utcfromtimestamp(int(ts_end/1000)).strftime('%Y-%m-%d %H:%M:%S')
             symbol = coin['symbol']
-            if gain <= -3 or gain >= 3:
-                msg = f"Binance minute monitoring\n\n{symbol=}\n{gain=}\n{ts=}\n{ts_end=}\n\nhttps://www.binance.com/en/trade/{coin['symbol'].replace('USDT', '_USDT')}?layout=pro&theme=dark"
+            if gain <= -3 or gain >= 3 or usd_vol_increase >= 500:
+                msg = f"Binance minute monitoring\n\nSymbol: {symbol}\nCost growth: {gain=}%\n" \
+                      f"Volume: {quote_asset_volume} USDT ({usd_vol_increase}%)\n" \
+                      f"Number of trades: {number_of_trades} ({number_of_trades_increase}%)\n" \
+                      f"UTC start: {ts}\nUTC end: {ts_end=}\n\n" \
+                      f"https://www.binance.com/en/trade/{coin['symbol'].replace('USDT', '_USDT')}" \
+                      f"?layout=pro&theme=dark"
                 tg_url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id=-1001706274303&text={msg}'
                 requests.get(url = tg_url)
             if gain >= best_gain[1]:
                 best_gain = (coin['symbol'], gain)
             print(f"{best_gain=} |> {symbol=}, {gain=}, {ts=}, {ts_end=}")
 
-            #sleep(1)
